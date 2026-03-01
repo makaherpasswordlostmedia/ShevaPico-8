7fcc3f4c4e05e093c0ded47aaea1d32fb697d64c
whyineedname <emssaoig1@gmail.com>
2026-03-01 21:29:35 +0200

    Add files via upload

--- src/Pico8Emulator.java
+++ src/Pico8Emulator.java
@@ -1,46 +1,13 @@
-import javax.microedition.midlet.*;
-import javax.microedition.lcdui.*;
-import java.io.*;
-
-/**
- * PICO-8 Emulator for J2ME
- * Базовая реализация эмулятора PICO-8 для J2ME телефонов
- */
-public class Pico8Emulator extends MIDlet {
-    private Display display;
-    private Pico8Canvas gameCanvas;
-    
-    public Pico8Emulator() {
-        super();
+public class Pico8Emulator {
+    private static Pico8Canvas gameCanvas;
+    public Pico8Emulator() {}
+    public static void main(String[] args) {
+        System.out.println("PICO-8 J2ME Emulator v1.0");
     }
-    
     public void startApp() {
-        if (display == null) {
-            display = Display.getDisplay(this);
-        }
-        
-        if (gameCanvas == null) {
-            gameCanvas = new Pico8Canvas(this);
-        }
-        
-        display.setCurrent(gameCanvas);
+        if (gameCanvas == null) gameCanvas = new Pico8Canvas(this);
         gameCanvas.start();
     }
-    
-    public void pauseApp() {
-        if (gameCanvas != null) {
-            gameCanvas.pause();
-        }
-    }
-    
-    public void destroyApp(boolean unconditional) {
-        if (gameCanvas != null) {
-            gameCanvas.stop();
-        }
-        notifyDestroyed();
-    }
-    
-    public void exitApp() {
-        destroyApp(true);
-    }
+    public void pauseApp() { if (gameCanvas != null) gameCanvas.pause(); }
+    public void destroyApp(boolean u) { if (gameCanvas != null) gameCanvas.stop(); System.exit(0); }
 }
